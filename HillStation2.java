18554532b83bc5c64f976cdc7b7afa6ff8e2b949
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-27 20:38:43 +0530

    replacing Skandagiri hillstation

---
+++ HillStation2.java
@@ -0,0 +1,22 @@
+class HillStation2{
+		public static void main(String []ar){ 
+			System.out.println("main() started");
+			int num=0;
+			
+			for(int i=0;i<ar.length;i++){
+			
+				if(num==8){
+				System.out.println();
+				num = 0;
+				}
+		ar[0] = "Skandhagiri HillStation";
+		System.out.print(ar[i]+"\t");
+		num = num+1;
+
+		}
+	}
+}
+				
+				
+			
+			
