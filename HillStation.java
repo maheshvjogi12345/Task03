4ee1e7c4210c448dd733a3b8f9b06fc4e6b510dc
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-27 20:38:05 +0530

    replacing nandhi hill

---
+++ HillStation.java
@@ -0,0 +1,25 @@
+class HillStation{
+		public static void main(String []ar){ 
+			System.out.println("main() started");
+			int num=0;
+			for(int i=0;i<ar.length;i++){
+			
+				if(num==8){
+				System.out.println();
+				num = 0;
+				}
+		ar[1] = "NandhiHill";
+		System.out.print(ar[i]+"\t");
+		num = num+1;
+
+		}
+	}
+}
+				
+				
+			
+			
+		
+		
+		
+		
